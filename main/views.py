cd24191158e94e54bd0b54a6eb18286908577659
Khalid <over13377@hotmail.com>
2026-01-05 16:18:48 +0300

    добавил views

--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
+from django.views.generic import TemplateView,DateDetailView
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+from .models import Category,Product,Size
+from django.db.models import Q
+
+class IndexView(TemplateView):
+    template_name ='main/base.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context = ['categories'] = Category.objects.all()
+        context = ['current_category'] = None
+        return context
+    
+
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.headers.get('HX-Request'):
+            return TemplateResponse(request,'main/home_content.html',context)
+        return TemplateResponse(request,self.template_name,context)
 
 # Create your views here.
